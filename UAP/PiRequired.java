0af8f40e6982b4077ead4f9dcb2d9e03df176271
Syumaylah <syumaylah1@gmail.com>
2025-05-31 20:24:14 +0700

    Update PiRequired.java

--- UAP/PiRequired.java
+++ UAP/PiRequired.java
@@ -1,5 +1,5 @@
 package uap.interfaces;
 
-public interface PiRequired {
-    double PI = 22.0 / 7.0;
+public interface PiRequired {  // interface untuk nilai pi
+    double PI = 22.0 / 7.0;  // pendekatan nilai pi (22/7)
 }
