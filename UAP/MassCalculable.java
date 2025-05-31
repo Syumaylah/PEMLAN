45891fbdf24778e6fc21c3de0fb6d52e73c6a6d2
Syumaylah <syumaylah1@gmail.com>
2025-05-31 20:23:33 +0700

    Update MassCalculable.java

--- UAP/MassCalculable.java
+++ UAP/MassCalculable.java
@@ -1,8 +1,8 @@
 package uap.interfaces;
 
-public interface MassCalculable {
-    int DENSITY = 8;
-    double THICKNESS = 0.5;
-
-    double getMass();
+public interface MassCalculable {  // interface untuk hitung massa
+    int DENSITY = 8;  // konstanta massa jenis (g/cm³)
+    double THICKNESS = 0.5;  // konstanta ketebalan (cm)
+    
+    double getMass();  // method untuk menghitung massa
 }
