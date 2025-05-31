3415503489e5637519b0265a75254b94bd26daf4
Syumaylah <syumaylah1@gmail.com>
2025-05-31 20:24:37 +0700

    Update Shape.java

--- UAP/Shape.java
+++ UAP/Shape.java
@@ -1,17 +1,17 @@
 package uap.bases;
 
-public abstract class Shape {
-    private String name;
+public abstract class Shape {  // kelas abstrak untuk bentuk dasar
+    private String name;  // variabel untuk menyimpan nama bentuk
 
-    public Shape() {}
+    public Shape() {}  // konstruktor default
 
-    public void setName(String inputName) {
-        this.name = inputName;
+    public void setName(String inputName) {  // method untuk mengatur nama
+        this.name = inputName;  // mengisi nilai name
     }
 
-    public String getName() {
-        return this.name;
+    public String getName() {  // method untuk mendapatkan nama
+        return this.name;  // mengembalikan nilai name
     }
 
-    public abstract void printInfo();
+    public abstract void printInfo();  // method abstrak untuk mencetak info
 }
